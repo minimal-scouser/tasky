1e5af9413b80c7d83fdade86cfe2b9b8e09fe125
scouser <throwawayforgithub@gmail.com>
2024-08-29 02:42:26 +0530

    configure testing and mui icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'v8',
+    },
+  },
 })
